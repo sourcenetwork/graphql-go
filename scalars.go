87b432f0f109bb20552c4c10d98c5e117b9c19ac
Chris Quigley <chris.quigley2@gmail.com>
2025-11-21 12:57:53 -0500

    Comment description; now -> UTC-NOW

--- scalars.go
+++ scalars.go
@@ -555,7 +555,9 @@ func unserializeDateTime(value interface{}) interface{} {
 var DateTime = NewScalar(ScalarConfig{
 	Name: "DateTime",
 	Description: "The `DateTime` scalar type represents a DateTime." +
-		" The DateTime is serialized as an RFC 3339 quoted string",
+		" The DateTime is serialized as an RFC 3339 quoted string." +
+		" A literal value of `NOW` is supported, and will be serialized " +
+		" as the current UTC time.",
 	Serialize:  serializeDateTime,
 	ParseValue: unserializeDateTime,
 	ParseLiteral: func(valueAST ast.Value, variables map[string]interface{}) interface{} {
@@ -564,8 +566,8 @@ var DateTime = NewScalar(ScalarConfig{
 			// Parse normal RFC3339 string
 			return unserializeDateTime(valueAST.Value)
 		case *ast.EnumValue:
-			// Support the literal `now`
-			if valueAST.Value == "now" {
+			// Support the literal `NOW`
+			if valueAST.Value == "NOW" {
 				return time.Now().UTC()
 			}
 		}
