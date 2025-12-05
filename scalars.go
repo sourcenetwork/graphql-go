0974704eeef9ebdf4f65a1e33a5ea57fb071e6e9
Chris Quigley <chris.quigley2@gmail.com>
2025-12-05 14:51:36 -0500

    Update comment

--- scalars.go
+++ scalars.go
@@ -561,9 +561,7 @@ var DateTime = NewScalar(ScalarConfig{
 	Description: "The `DateTime` scalar type represents a DateTime." +
 		" The DateTime is serialized as an RFC 3339 quoted string." +
 		" A literal value of `UTC_NOW` is supported, and will be serialized " +
-		" as the current UTC time. Note that this will be resolved as the UTC time on" +
-		" the server's side. It may not exactly match the resolution of time.Now().UTC()" +
-        " on the client's machine at the time of sending the request.",	
+		" as a string `UTC_NOW` which can be handled by the consumer." +
 	Serialize:  serializeDateTime,
 	ParseValue: unserializeDateTime,
 	ParseLiteral: func(valueAST ast.Value, variables map[string]interface{}) interface{} {
